0095f77efe332a359c4650492799123ef90044df
Jim Kang <jimkang@gmail.com>
2014-04-01 20:00:14 -0400

    Initial commit. Tree showing, no zoom or colors.

---
+++ example/example-quadtree.js
@@ -0,0 +1,34 @@
+function exampleQuadtree(width, height, points) {
+  var treeFactory = d3.geom.quadtree()
+    .extent([[-1, -1], [width + 1, height + 1]]);
+
+  var xFactor = (width > height) ? 1 : height/width;
+  var yFactor = (height > width) ? 1 : width/height;
+
+  function getX(d) {
+    return d[0] * xFactor;
+  };
+
+  function getY(d) {
+    return d[1] * yFactor;
+  }
+
+  treeFactory.x(getX);
+  treeFactory.y(getY);
+
+  var quadtree = treeFactory(points);
+
+  quadtree.visit(function addLabel(node, x1, y1, x2, y2) {
+    var labelPrefix = 'quad_';
+
+    if (node.leaf) {
+      node.label = 'point_' + node.point[0] + '_' + node.point[1];
+    }
+    else {
+      node.label = 'quad_' + x1 + '_' + y1;
+    }
+  });
+
+  return quadtree;
+}
+
---
+++ example/exhibit-controller.js
@@ -0,0 +1,121 @@
+function exhibitController() {
+  var exhibit = {
+    points: [],
+    displayedPointRange: [0, 100],
+    maxNumberOfPoints: 1000,
+    padding: 8,
+    detailsBox: d3.select('.details-box')
+  };
+
+  var boardWidth = 0;
+  var boardHeight = 0;
+
+  ((function captureBoardDimensions() {
+    var boardEl = d3.select('#quadtreetree').node();
+
+    boardWidth = boardEl.clientWidth;
+    if (boardWidth < 1) {
+      // This is necessary on Firefox.
+      boardWidth = boardEl.parentElement.clientWidth;
+    }
+
+    boardHeight = boardEl.clientHeight;
+    if (boardHeight < 1) {
+      // This is necessary on Firefox.
+      boardHeight = boardEl.parentElement.clientHeight;
+    }
+
+    boardWidth -= (2 * exhibit.padding);
+    boardHeight -= (2 * exhibit.padding);
+  })());
+
+  function createPointRandomly() {
+    return [
+      ~~(Math.random() * boardWidth),
+      ~~(Math.random() * boardHeight)
+    ];
+  }
+
+  exhibit.displayedPoints = function displayedPoints() {
+    return exhibit.points.slice(
+      exhibit.displayedPointRange[0], exhibit.displayedPointRange[1]);
+  };
+
+  exhibit.points = d3.range(exhibit.maxNumberOfPoints).map(createPointRandomly);
+  // exhibit.points = [
+  //   [mapWidth - 1, mapHeight - 1], 
+  //   [mapWidth - 10, mapHeight - 10], 
+  //   [mapWidth - 100, mapHeight - 100]
+  // ];
+
+  exhibit.quadtree = exampleQuadtree(boardWidth, boardHeight, 
+    exhibit.displayedPoints());
+
+  exhibit.quadtreetree = quadtreetree({
+    boardSelection: d3.select('#quadtreetree'),
+    rootSelection: d3.select('#treeroot')
+  });
+  exhibit.quadtreetree.update(exhibit.quadtree);
+  // exhibit.quadmap = quadtreeMap({
+  //   x: exhibit.padding, 
+  //   y: exhibit.padding, 
+  //   width: mapWidth, 
+  //   height: mapHeight, 
+  //   quadtree: exhibit.quadtree, 
+  //   rootSelection: d3.select('#quadroot')
+  // });
+
+  // renderQuadtreePoints({
+  //   points: exhibit.displayedPoints(),
+  //   rootSelection: d3.select('#pointroot'),
+  //   x: exhibit.padding,
+  //   y: exhibit.padding,
+  //   width: mapWidth, 
+  //   height: mapHeight,
+  // });
+
+  // document.addEventListener('quadtreemap-quadSelected', reportSelectedQuad);
+  // document.addEventListener('quadtreepoints-pointSelected', reportSelectedPt);
+
+  // function reportSelectedQuad(e) {
+  //   var quad = e.detail;
+  //   var quadtreenode = 
+  //     cleanNodeForPresentation(decircularizeQuadtreeNode(quad.quadtreenode));
+  //   exhibit.detailsBox.text(JSON.stringify(quadtreenode, null, '  '));
+  // }
+
+  // function reportSelectedPt(e) {
+  //   var point = e.detail;
+  //   exhibit.detailsBox.text(JSON.stringify(point));    
+  // }
+
+  function decircularizeQuadtreeNode(node) {
+    var safeNode = _.omit(node, 'nodes', 'parent');
+
+    if (node.nodes) {
+      safeNode.nodes = node.nodes.map(function cleanChild(child) {
+        return _.omit(child, 'nodes', 'parent');
+      });
+    }
+    return safeNode;
+  }
+
+  // Assumes circular refs have been removed from node.
+  function cleanNodeForPresentation(node) {
+    ['point', 'x', 'y'].forEach(function removePropertyIfEmpty(property) {
+      if (!node[property]) {
+        delete node[property];
+      }
+      if (node.nodes) {
+        node.nodes.forEach(cleanNodeForPresentation);
+      }
+    });
+    return node;
+  }
+ 
+  return exhibit;
+}
+
+
+var theExhibit = exhibitController();
+
