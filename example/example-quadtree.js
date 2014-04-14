a6de8378e183a48cc249ac3f2545eee3e83213d6
Jim Kang <jimkang@gmail.com>
2014-04-14 14:08:00 -0400

    - Updated quadtreeToLayoutTree to set leaf and ghost properties instead of color. - Removed setColor and combineVisitFunctions from exampleQuadtree. - Updated quadtreetree.syncDOMToNodes to use leaf and ghost properties of the layout node to set classes on the DOM element instead of setting the fill directly. - Added CSS classes to color the leaf and ghost classes.

--- example/example-quadtree.js
+++ example/example-quadtree.js
@@ -15,31 +15,12 @@ function exampleQuadtree(width, height, points) {
 
   treeFactory.x(getX);
   treeFactory.y(getY);
-
   var quadtree = treeFactory(points);
-
-  function setColor(node, x1, y1, x2, y2) {
-    if (node.leaf) {
-      node.color = 'red';
-    }
-    else {
-      node.color = 'orange';        
-    }
-  }
-
-  function combineVisitFunctions() {
-    var fns = arguments;
-    return function combinedVisit(node, x1, y1, x2, y2) {
-      for (var i = 0; i < fns.length; ++i) {
-        fns[i](node, x1, y1, x2, y2);
-      }
-    };
-  }
 
   var labeler = createQuadtreeLabeler('tree-');
 
   quadtree.updateNodes = function updateNodes() {    
-    quadtree.visit(combineVisitFunctions(labeler.setLabelOnNode, setColor));
+    quadtree.visit(labeler.setLabelOnNode);
   };
 
   quadtree.updateNodes();
